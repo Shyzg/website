f716a064f068830f2b098150f2e5d85136527788
Al Jarreau Arman Maulana <shyzagonakamoto@gmail.com>
2022-04-29 00:48:10 +0700

    🚀 Updating App.js.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1 className="text-3xl font-bold">
-                    Belom jadi
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload. Belom jadi —Shyzago Nakamoto.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+                <h1 className="text-3xl font-bold underline">
+                    Hello world!
                 </h1>
             </header>
         </div>
